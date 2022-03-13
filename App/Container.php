87055dec134d8c5ba82452facceb18f74efc4b14
Albert Stepantsev <albert.stepantsev@sprintf.ru>
2022-03-13 18:09:45 +0300

    Step 7

--- App/Container.php
+++ App/Container.php
@@ -7,23 +7,45 @@ class Container
 
     private array $objects = [];
 
-    public function __construct()
-    {
-        $this->objects = [
-            Db::class => fn() => new Db(),
-            UserRepository::class => fn() => new UserRepository($this->get(Db::class)),
-            UserController::class => fn() => new UserController($this->get(UserRepository::class)),
-        ];
-    }
-
     public function has(string $id): bool
     {
-        return isset($this->objects[$id]);
+        return isset($this->objects[$id]) || class_exists($id);
     }
 
     public function get(string $id): mixed
     {
-        return $this->objects[$id]();
+        return isset($this->objects[$id]) ? $this->objects[$id]() : $this->prepareObject($id);
+    }
+
+    private function prepareObject(string $class): object
+    {
+        $classReflector = new \ReflectionClass($class);
+
+        // Получаем рефлектор конструктора класса, проверяем - есть ли конструктор
+        // Если конструктора нет - сразу возвращаем экземпляр класса
+        $constructReflector = $classReflector->getConstructor();
+        if (empty($constructReflector)) {
+            return new $class;
+        }
+
+        // Получаем рефлекторы аргументов конструктора
+        // Если аргументов нет - сразу возвращаем экземпляр класса
+        $constructArguments = $constructReflector->getParameters();
+        if (empty($constructArguments)) {
+            return new $class;
+        }
+
+        // Перебираем все аргументы конструктора, собираем их значения
+        $args = [];
+        foreach ($constructArguments as $argument) {
+            // Получаем тип аргумента
+            $argumentType = $argument->getType()->getName();
+            // Получаем сам аргумент по его типу из контейнера
+            $args[$argument->getName()] = $this->get($argumentType);
+        }
+
+        // И возвращаем экземпляр класса со всеми зависимостями
+        return new $class(...$args);
     }
 
 }
